56378d0c352aad417836b687dfaa36ad2c279233
Oksana Bondareva <oksana20-89@mail.ru>
2024-04-06 12:43:57 +0300

    fix: correct solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -65,7 +65,7 @@ function getPromiseResult(source) {
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
 function getFirstResolvedPromiseResult(promises) {
-  return Promise.race(promises);
+  return Promise.any(promises);
 }
 
 /**
@@ -88,7 +88,7 @@ function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
 function getFirstPromiseResult(promises) {
-  return Promise.any(promises);
+  return Promise.race(promises);
 }
 
 /**
